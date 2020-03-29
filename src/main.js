b7f551208064cea82178bc38d99648eda865f5b0
cjh <S011300407>
2020-03-29 21:41:22 +0800

    页面搭建

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
 import Router from './router'
+import './assets/newcss/base.css'
+import './assets/newcss/index.css'
 
 Vue.use(VueAxios, Axios)
 
