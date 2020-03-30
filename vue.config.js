a45cd3c022d054e89a4f773dafa746872a503967
cjh <S011300407>
2020-03-30 19:39:46 +0800

    购物车功能实现

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
     publicPath: "",   //可以配置如/api/v0/xx
     devServer: {
         host: 'localhost',
-        port: 9090,
+        port: 8080,
         proxy:{
             '/mock':{
-                target:"http://localhost:9090",
+                target:"http://localhost:8080",
                 ws:false,   //websocket不知道干啥的
                 changeOrigin: false //需添加,与路径有关
             }
